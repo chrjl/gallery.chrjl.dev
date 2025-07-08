fc5b0a5895d64b60fc7bea0588840dd56fac43aa
chrjl <73675826+chrjl@users.noreply.github.com>
2025-07-07 22:47:28 -0700

    build: install components for building markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import markdownIntegration from '@astropub/md';
+import externalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  integrations: [react(), markdownIntegration()],
+  markdown: {
+    rehypePlugins: [[externalLinks, { rel: ['noreferrer'], target: '_blank' }]],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
